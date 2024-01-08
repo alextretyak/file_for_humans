4c863ad9806b8da7a13dcc039bd832d140393a45
Alexander Tretyak <alextretyak@users.noreply.github.com>
2024-01-08 14:24:27 +1000

    eof_reached() -> eof_passed()

--- IFile.hpp
+++ IFile.hpp
@@ -17,18 +17,20 @@ public:
     /*
     `at_eof()` function works like `eof()` in Pascal, i.e. it returns true if the file is at the end.
     The name `at_the_end()` for this function has been rejected as it is longer, and the name `at_eof()` is still quite correct:
-    >[http://www.irietools.com/iriepascal/progref177.html <- google:‘pascal eof’]:‘file is at end-of-file’
+    >[http://www.irietools.com/iriepascal/progref177.html <- google:‘pascal eof’][-1]:‘file is at end-of-file’
     */
     bool at_eof()
     {
     }
 
     /*
-    `eof_reached()` function works like `eof()` in C, i.e. it returns true if a read operation has attempted to read past the end of the file.
+    `eof_passed()` function works like `eof()` in C, i.e. it returns true if a read operation has attempted to read past the end of the file.
     The name of this function was inspired by:
-    >[https://en.cppreference.com/w/cpp/io/basic_ios/eof]:‘reached end-of-file’
+    >[https://learn.microsoft.com/en-us/cpp/c-runtime-library/reference/feof]:‘the end of `stream` has been passed.’
+    The name `eof_reached()` for this function has been rejected because it is confusing:
+    >[-1]:‘The *‘eof’ function is used to determine when the end of the text file has been reached.’
     */
-    bool eof_reached()
+    bool eof_passed()
     {
     }
 
